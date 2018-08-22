de5c77cae37a6750a424d71709517a20c24acbed
Achirawat Juasawad <achirawat@angstromsolutions.net>
2018-08-22 17:38:58 +0700

    .

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+          <input type="text" placeholder="Say Something" />
+          <p className="echo">Echo:</p>
+          <p>This should mirror the text you typed into the input field.</p>
+        </div>
       </div>
     );
   }
